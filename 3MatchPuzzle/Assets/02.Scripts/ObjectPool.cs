3b9cb08f07fc1827b37a34e5c20d5bcd4ec81821
YoonHeedo <yoon0020@naver.com>
2020-11-17 18:29:22 +0900

    오브젝트 풀링 스크립트 추가

---
+++ 3MatchPuzzle/Assets/02.Scripts/ObjectPool.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public static ObjectPool Instance;
+
+    public GameObject poolingObjectPrefabs;
+
+    private Queue<GameObject> poolingObjectQueue = new Queue<GameObject>();
+
+    private void Awake()
+    {
+        Instance = this;
+        //Initialize(10);
+    }
+
+   GameObject CreatenewObject()
+    {
+        var newObj = Instantiate(poolingObjectPrefabs, transform);
+        newObj.SetActive(false);
+        return newObj;
+    }
+
+    void Initialize(int count)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            poolingObjectQueue.Enqueue(CreatenewObject());
+        }
+    }
+
+    public static GameObject GetObject()
+    {
+        if(Instance.poolingObjectQueue.Count>0)
+        {
+            var obj = Instance.poolingObjectQueue.Dequeue();
+            obj.transform.SetParent(null);
+            obj.gameObject.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            var newobj = Instance.CreatenewObject();
+            newobj.transform.SetParent(null);
+            newobj.gameObject.SetActive(true);
+            return newobj;
+        }
+    }
+
+    public static void ReturnObject(GameObject obj)
+    {
+        obj.SetActive(false);
+        obj.transform.SetParent(Instance.transform);
+        Instance.poolingObjectQueue.Enqueue(obj);
+    }
+}
