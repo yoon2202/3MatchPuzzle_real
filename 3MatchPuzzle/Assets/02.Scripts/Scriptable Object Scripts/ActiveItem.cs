eec307994f6d27ec00a401d27ddb37f1fc25af67
yoon2202 <34045841+yoon2202@users.noreply.github.com>
2020-12-08 23:42:27 +0900

    업그레이드 구현중

---
+++ 3MatchPuzzle/Assets/02.Scripts/Scriptable Object Scripts/ActiveItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "ActiveItem", menuName = "Active")]
+public class ActiveItem : ScriptableObject
+{
+    public string ActiveName;
+    public Sprite Image;
+    public string ActiveContent;
+    public int CurrentLevel;
+    public int MaxLevel;
+    public int Baseprice;
+
+}
