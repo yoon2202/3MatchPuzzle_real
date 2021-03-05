ff212eebc7bd6a49a47592dbaf779f1b62e33de3
yoon2202 <34045841+yoon2202@users.noreply.github.com>
2021-03-05 18:57:06 +0900

    Update

---
+++ 3MatchPuzzle/Assets/02.Scripts/Main/ScrollManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ScrollManager : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Scrollbar scrollbar;
+    public Transform contentTr;
+
+    [SerializeField]
+    private Slider TabSlider;
+    [SerializeField]
+    private RectTransform[] BtnRect;
+
+    const int SIZE = 4;
+    float[] pos = new float[SIZE];
+    float distance, targetPos, currentPos;
+    private int targetIndex;
+
+    bool b_Drag;
+
+    void Start()
+    {
+        distance = 1f / (SIZE - 1);
+
+        for (int i = 0; i < SIZE; i++)
+            pos[i] = distance * i;
+    }
+
+    float setPos()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            if (scrollbar.value < pos[i] + distance * 0.5f && scrollbar.value > pos[i] - distance * 0.5f)
+            {
+                targetIndex = i;
+                return pos[i];
+            }
+        }
+        return 0;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        currentPos = setPos();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        b_Drag = true;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        b_Drag = false;
+        targetPos = setPos();
+
+        // 절반 거리를 드레그 하지 않았을 경우
+        if(currentPos == targetPos)
+        {
+            if(eventData.delta.x > 18 && currentPos - distance >= 0) // 왼쪽으로 드레그
+            {
+                --targetIndex;
+                targetPos = currentPos - distance;
+            }
+            else if(eventData.delta.x < -18 && currentPos + distance <= 1.01f) // 오른쪽으로 드레그
+            {
+                ++targetIndex;
+                targetPos = currentPos + distance;
+            }
+        }
+
+        for(int i = 0; i< SIZE; i++)
+        {
+            if (contentTr.GetChild(i).GetComponent<VerticalScroll>() && currentPos != pos[i] && targetPos == pos[i])
+            {
+                contentTr.GetChild(i).GetChild(1).GetComponent<Scrollbar>().value = 1;
+            }
+        }
+
+    }
+
+    void Update()
+    {
+        TabSlider.value = scrollbar.value;
+
+        if (b_Drag == false)
+        {
+            scrollbar.value = Mathf.Lerp(scrollbar.value, targetPos, 0.1f);
+
+            for(int i = 0; i< SIZE; i++)
+            {
+                BtnRect[i].sizeDelta = new Vector2(i == targetIndex ? 360 : 180, BtnRect[i].sizeDelta.y);
+            }
+
+        }
+
+    }
+
+
+    public void TabClick(int n)
+    {
+        targetIndex = n;
+        targetPos = pos[n];
+    }
+
+
+}
---
+++ 3MatchPuzzle/Assets/02.Scripts/Main/VerticalScroll.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VerticalScroll : ScrollRect
+{
+    bool forParent;
+    ScrollManager scrollManager;
+    ScrollRect parent_ScrollRect;   
+
+    protected override void Start()
+    {
+        scrollManager = FindObjectOfType<ScrollManager>();
+        parent_ScrollRect = scrollManager.GetComponent<ScrollRect>();
+    }
+
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        forParent = Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y);
+
+        if (forParent)
+        {
+            scrollManager.OnBeginDrag(eventData);
+            parent_ScrollRect.OnBeginDrag(eventData);
+        }
+        else
+        {
+            base.OnBeginDrag(eventData);
+        }
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        if(forParent)
+        {
+            scrollManager.OnDrag(eventData);
+            parent_ScrollRect.OnDrag(eventData);
+        }
+        else
+        {
+            base.OnDrag(eventData);
+        }
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        if (forParent)
+        {
+            scrollManager.OnEndDrag(eventData);
+            parent_ScrollRect.OnEndDrag(eventData);
+        }
+        else
+        {
+            base.OnEndDrag(eventData);
+        }
+    }
+}
